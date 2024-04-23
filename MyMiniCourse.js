127881d2e14f28ce3acc368fbe1a50bbd2ec02f5
Lange <langescomputerscience@gmail.com>
2024-04-23 13:35:36 -0400

    Initial MyMiniCourse Part II COmmit

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Mr. Lange
+// Team Member: 
+// Bolt ID: 6B78
+// Date: 4.23.2024
+// Program: My Mini Course
+// Program URL: https://edu.sphero.com/program/16743345/edit
+
+async function startProgram() {
+	// Write code here
+}
