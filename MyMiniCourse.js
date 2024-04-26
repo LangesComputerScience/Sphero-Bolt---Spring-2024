6aeeebd87ef60009d80ae175f5413c1dacee7bc8
Lange <langescomputerscience@gmail.com>
2024-04-26 11:38:23 -0400

    Mr. Lange - Checkpoint 3 - Green LED

--- MyMiniCourse.js
+++ MyMiniCourse.js
@@ -6,15 +6,24 @@
 // Program URL: https://edu.sphero.com/program/16743345/edit
 
 async function startProgram() {
-	// Mr. Lange
+	// Mr. Lange - Checkpoint One - Blue
+	setMainLed({ r: 0, g: 0, b: 255 });
 	await roll((getHeading() + 0), 60, 2);
 	await delay(2);
 	await roll((getHeading() + 90), 60, 2);
 	await delay(2);
 
-	// Michael Jordan
+	// Michael Jordan - Checkpoint Two + Audio
+	await Sound.ScienceFiction.Blaster.play(true);
 	await roll((getHeading() + 125), 60, 5);
 	await delay(2);
 	await roll((getHeading() + 320), 60, 2);
+	await delay(2);
+
+	// Mr. Lange - Checkpoint Three - Green
+	setMainLed({ r: 255, g: 0, b: 0 });
+	await roll((getHeading() + 0), 60, 2);
+	await delay(2);
+	await roll((getHeading() + 90), 60, 2);
 	await delay(2);
 }
