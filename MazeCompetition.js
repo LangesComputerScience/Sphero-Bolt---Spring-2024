f01ad5b02ddd4232c4ceec30a30ee6551eea8a6d
Lange <langescomputerscience@gmail.com>
2024-04-30 13:28:28 -0400

    Added Beginning Comments & Start Program JS

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Mr. Lange
+// Team Member: 
+// Bolt ID: 6B78
+// Date: 4.30.2024
+// Program: Maze Competition
+// Program URL: https://edu.sphero.com/program/16788349/edit
+
+async function startProgram() {
+	// Write code here
+}
